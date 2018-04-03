4e5b4a295ef631d9ce8632a5f4d83d3bf34e756b
Catherine Safrit <34667751+csafrit@users.noreply.github.com>
2018-04-03 00:43:11 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+consumer_key = "krTwI9nxABqz3Nf35ecivvP4M"
+consumer_secret = "kNDl2B4wsZZt2x1QEaWG3LZWrZfpnP0w8OxerX09Tgpudpoem9"
+access_token = "431825981-GNgiHoexEsAsNeQLzHXL3bQboSFNT57lRjw3bYc8"
+access_token_secret = "a6htV8sPPsdzoV2TpcP70hOISw6tE1W7Igv4z1AXylIYB"
+
